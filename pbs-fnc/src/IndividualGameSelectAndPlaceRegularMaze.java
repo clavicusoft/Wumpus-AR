2eccdb3d4176efa26168e442a9771cf5a8945a68
carlos-gamboa <cali1997gv@hotmail.com>
2017-11-05 21:35:48 -0600

    Añadir el código de las pruebas funcionales a la carpeta correcta

---
+++ pbs-fnc/src/IndividualGameSelectAndPlaceRegularMaze.java
@@ -0,0 +1,83 @@
+package com.clavicusoft.wumpus.Select;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.clavicusoft.wumpus.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@RunWith(AndroidJUnit4.class)
+public class IndividualGameSelectAndPlaceRegularMaze {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void selectAndPlaceRegularMaze() {
+        try {
+            Thread.sleep(12000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ViewInteraction button = onView(
+                allOf(ViewMatchers.withId(R.id.Individual), withText("Individual"),
+                        withParent(withId(R.id.linearLayout)),
+                        isDisplayed()));
+        button.perform(click());
+
+        ViewInteraction viewPager = onView(
+                allOf(withId(R.id.ImageSlider), isDisplayed()));
+        viewPager.perform(swipeLeft());
+
+        ViewInteraction viewPager2 = onView(
+                allOf(withId(R.id.ImageSlider), isDisplayed()));
+        viewPager2.perform(swipeLeft());
+
+        ViewInteraction viewPager3 = onView(
+                allOf(withId(R.id.ImageSlider), isDisplayed()));
+        viewPager3.perform(swipeLeft());
+
+        ViewInteraction viewPager4 = onView(
+                allOf(withId(R.id.ImageSlider), isDisplayed()));
+        viewPager4.perform(swipeLeft());
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.bttnStartGame), withText("Iniciar el juego"), isDisplayed()));
+        button2.perform(click());
+
+        ViewInteraction button3 = onView(
+                allOf(withId(R.id.buttonMyLocation), withText("Agregar ubicación"), isDisplayed()));
+        button3.perform(click());
+
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction button5 = onView(
+                allOf(withId(R.id.bcontinuar), withText("¡Continuar!"), isDisplayed()));
+        button5.perform(click());
+
+    }
+
+}
---
+++ pbs-fnc/src/MultiplayerGame.java
@@ -0,0 +1,71 @@
+package com.clavicusoft.wumpus.Select;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+
+import com.clavicusoft.wumpus.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MultiplayerGame {
+
+    @Rule
+    public ActivityTestRule<IntroAnimation> mActivityTestRule = new ActivityTestRule<>(IntroAnimation.class);
+
+    @Test
+    public void multiplayerGame() {
+        try {
+            Thread.sleep(12000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ViewInteraction button = onView(
+                allOf(ViewMatchers.withId(R.id.buttonMultijuador), withText("Multijugador"),
+                        withParent(withId(R.id.linearLayout)),
+                        isDisplayed()));
+        button.perform(click());
+
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
